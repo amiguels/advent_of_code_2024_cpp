a1d8a38d91eca1be08b870e4cc2b9936eeec07af
Miguel Sousa <miguel.bms@gmail.com>
2024-12-02 21:49:39 +0000

    Speed it up a bit.

--- day_2/main.cpp
+++ day_2/main.cpp
@@ -7,25 +7,20 @@
 #include <cmath>
 #include <algorithm>
 
-bool check(auto& levels){
-    //  x < 0 ? true : false
-    bool direction{std::signbit(static_cast<int32_t>(levels.at(0) - static_cast<int32_t>(levels.at(1))))}; 
+bool check(std::vector<int32_t>& levels){
+    const bool direction{std::signbit(levels.at(0) - levels.at(1))}; 
     
     for (auto it = levels.begin(); it < levels.end() - 1; it++ )
     {
-        auto& this_level = *it;
-        auto& next_level = *(it+1);
+        const int32_t delta = (*it) - (*(it+1));
 
-        int32_t delta = static_cast<int32_t>(this_level) - static_cast<int32_t>(next_level);
-
-        bool next_direction{std::signbit(delta)};
-        if( direction != next_direction )
+        if( direction != std::signbit(delta) )
         {
             // std::cout << "Direction check failed at {" << this_level << "} and {" << next_level << "}. Expected " << direction << " but got " << next_direction << "." << std::endl;
             return false;
         }
 
-        int32_t abs_delta = std::abs(delta);
+        const int32_t abs_delta = std::abs(delta);
 
         if( abs_delta < 1 || abs_delta > 3)
         {
@@ -37,15 +32,13 @@ bool check(auto& levels){
     return true;
 }
 
-bool check_reduced(auto& levels){
+bool check_reduced(std::vector<int32_t>& levels){
     for (uint32_t index_to_avoid = 0; index_to_avoid < levels.size() ; index_to_avoid++)
     {
-        std::vector<uint32_t> reduced_vector{levels};
+        std::vector<int32_t> reduced_vector{levels};
         reduced_vector.erase(reduced_vector.begin() + index_to_avoid);
 
-        bool are_reduced_levels_ok{check(reduced_vector)};
-
-        if (are_reduced_levels_ok)
+        if (check(reduced_vector))
         {
             return true;
         }
@@ -61,6 +54,7 @@ int main(int argc, char* argv[]){
         std::cerr << "Incorrect number of arguments. Usage: " << argv[0] << " <input_file_path>" << std::endl;
         return 1;
     }
+
     std::string input_file_path = argv[1];
 
     std::ifstream file(input_file_path);
@@ -70,7 +64,7 @@ int main(int argc, char* argv[]){
     }
     
     std::string input_line;
-    std::vector<uint32_t> levels{}; 
+    std::vector<int32_t> levels{}; 
 
     uint32_t ok_count{0};
     uint32_t ok_reduced_count{0};
@@ -85,15 +79,20 @@ int main(int argc, char* argv[]){
             levels.push_back(std::stoi(str));
         }
 
-        bool check_is_ok{check(levels)};
-        ok_count += check_is_ok;
-
-        if (not check_is_ok)
+        // This loop is only for easy benchmarking.
+        for (size_t i = 0; i < 1; i++)
         {
-            check_is_ok = check_reduced(levels);
-        }
-        
-        ok_reduced_count += check_is_ok;       
+            bool check_is_ok{check(levels)};
+            ok_count += check_is_ok;
+
+            if (not check_is_ok)
+            {
+                check_is_ok = check_reduced(levels);
+            }
+            
+            ok_reduced_count += check_is_ok;
+            
+        } 
     }
 
     std::cout << "Counted " << ok_count << " good reports." << std::endl;  
