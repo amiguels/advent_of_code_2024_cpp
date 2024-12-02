a9504ffbddb03633d33df08923d3404335a21c94
Miguel Sousa <miguel.bms@gmail.com>
2024-12-02 09:35:32 +0000

    Day 2.

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 Solved in C++.
 
-Each day contains a makefile to compile and run the code with
+Each day folder contains a makefile to compile and run the code with
 `make` or `make test`, depending on wether you'd like to use the full input or the test input.
+
+Please be aware that the solutions found in this repo are not necessarily the fastest or most optimal ones. I am simply using these challenges as an opportunity to explore new languages features, so I may opt for trying something new (to me), instead of pushing for the fastest routine.
---
+++ day_2/main.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+#include <ranges>
+#include <vector>
+#include <sstream>      // std::istringstream
+#include <cmath>
+#include <algorithm>
+
+bool check(auto& levels){
+    //  x < 0 ? true : false
+    bool direction{std::signbit(static_cast<int32_t>(levels.at(0) - static_cast<int32_t>(levels.at(1))))}; 
+    
+    for (auto it = levels.begin(); it < levels.end() - 1; it++ )
+    {
+        auto& this_level = *it;
+        auto& next_level = *(it+1);
+
+        int32_t delta = static_cast<int32_t>(this_level) - static_cast<int32_t>(next_level);
+
+        bool next_direction{std::signbit(delta)};
+        if( direction != next_direction )
+        {
+            // std::cout << "Direction check failed at {" << this_level << "} and {" << next_level << "}. Expected " << direction << " but got " << next_direction << "." << std::endl;
+            return false;
+        }
+
+        int32_t abs_delta = std::abs(delta);
+
+        if( abs_delta < 1 || abs_delta > 3)
+        {
+            // std::cout << "Delta check failed at {" << this_level << "} and {" << next_level << "}: " << abs_delta << "." << std::endl;
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool check_reduced(auto& levels){
+    for (uint32_t index_to_avoid = 0; index_to_avoid < levels.size() ; index_to_avoid++)
+    {
+        std::vector<uint32_t> reduced_vector{levels};
+        reduced_vector.erase(reduced_vector.begin() + index_to_avoid);
+
+        bool are_reduced_levels_ok{check(reduced_vector)};
+
+        if (are_reduced_levels_ok)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main(int argc, char* argv[]){
+
+    // Get input file path and open it
+    if (argc != 2) {
+        std::cerr << "Incorrect number of arguments. Usage: " << argv[0] << " <input_file_path>" << std::endl;
+        return 1;
+    }
+    std::string input_file_path = argv[1];
+
+    std::ifstream file(input_file_path);
+    if (!file.is_open()) {
+        std::cerr << "Error opening file: " << input_file_path << std::endl;
+        return 1;
+    }
+    
+    std::string input_line;
+    std::vector<uint32_t> levels{}; 
+
+    uint32_t ok_count{0};
+    uint32_t ok_reduced_count{0};
+
+    while (std::getline(file, input_line)) {
+        levels.clear();
+        std::istringstream stream(input_line);
+
+        std::string str{};
+        while (stream >> str)
+        {
+            levels.push_back(std::stoi(str));
+        }
+
+        bool check_is_ok{check(levels)};
+        ok_count += check_is_ok;
+
+        if (not check_is_ok)
+        {
+            check_is_ok = check_reduced(levels);
+        }
+        
+        ok_reduced_count += check_is_ok;       
+    }
+
+    std::cout << "Counted " << ok_count << " good reports." << std::endl;  
+    std::cout << "Counted " << ok_reduced_count << " good reduced reports." << std::endl;  
+    
+}
