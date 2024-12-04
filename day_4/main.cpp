0034f19f602d54e39f52a9f1686bd2bcac4dfdbc
Miguel Sousa <miguel.bms@gmail.com>
2024-12-04 23:01:57 +0000

    Changed to only contiguous letters.

--- day_4/main.cpp
+++ day_4/main.cpp
@@ -10,51 +10,117 @@
 
 using namespace std;
 
-static constexpr char XMAS[] = {'X', 'M', 'A', 'S'};
-static constexpr size_t LAST_CHAR_INDEX = (sizeof(XMAS) / sizeof(char))-1;
+// static constexpr char XMAS[] = {'X', 'M', 'A', 'S'};
+// static constexpr size_t LAST_CHAR_INDEX = (sizeof(XMAS) / sizeof(char))-1;
+
+// size_t countOccurrencesInLine(vector<char>::iterator it, vector<char>::iterator end_it, size_t char_idx)
+// {
+//     size_t count{0};
+
+//     if (it == end_it)
+//     {
+//         return count;
+//     }
+    
+//     while(it != end_it){
+//         // cout << "Looking for " << XMAS[char_idx] << " starting at idx " << distance(line.begin(), it) << endl;
+//         it = find(it, end_it, XMAS[char_idx]);
+
+//         if (char_idx == LAST_CHAR_INDEX)
+//         {
+//             if( it != end_it )
+//             {
+//                 // cout << "Found one!" << endl;
+//                 it++;
+//                 count++;
+//             }
+//             else
+//             {
+//                 return count;
+//             }
+//         }
+//         else
+//         {
+//             if( it != end_it )
+//             {
+//                 count += countOccurrencesInLine(it+1, end_it, char_idx+1);
+//                 it++;
+//             }
+//             else
+//             {
+//                 return count;
+//             }
+//         }
+//     }
+
+//     return count;
+// }
 
 
-size_t countOccurrences(vector<char>& line, vector<char>::iterator it, size_t char_idx)
+static const vector<char> XMAS = {'X', 'M', 'A', 'S'};
+
+size_t countOccurrencesInLine(vector<char>& line)
 {
     size_t count{0};
-
-    if (it == line.end())
-    {
-        return count;
-    }
     
-    while(it != line.end()){
-        // cout << "Looking for " << XMAS[char_idx] << " starting at idx " << distance(line.begin(), it) << endl;
-        it = find(it, line.end(), XMAS[char_idx]);
+    auto it = line.begin();
 
-        if (char_idx == LAST_CHAR_INDEX)
+    while(it != line.end()){
+       
+        it = std::search(it, line.end(), XMAS.begin(), XMAS.end());
+        if (it != line.end())
         {
-            if( it != line.end() )
-            {
-                // cout << "Found one!" << endl;
-                it++;
-                count++;
-            }
-            else
-            {
-                return count;
-            }
-        }
-        else
-        {
-            if( it != line.end() )
-            {
-                count += countOccurrences(line, it+1, char_idx+1);
-                it++;
-            }
-            else
-            {
-                return count;
-            }
+            count++;
+            it += XMAS.size();
         }
     }
 
     return count;
+}
+
+size_t countOccurrencesInDatabase(vector<vector<char>> vec_of_lines){
+    size_t count{0};
+
+    for (auto& line : vec_of_lines)
+    {      
+        size_t new_count = countOccurrencesInLine(line);
+        // cout << "Found " << new_count << endl;
+        count += new_count; 
+
+        auto reversed_line = vector<char>(line.rbegin(), line.rend());
+        new_count = countOccurrencesInLine(reversed_line);
+        // cout << "Found " << new_count << endl;
+        count += new_count; 
+    }
+
+    return count;
+}
+
+vector<vector<char>> getVerticalMatrixFrom(vector<vector<char>> matrix)
+{
+    vector<vector<char>> new_matrix{};
+    
+    for (size_t i = 0; i < matrix.size(); ++i) {
+        new_matrix.push_back({});
+
+        for (size_t j = 0; j < matrix[i].size(); ++j) {
+            new_matrix[new_matrix.size()-1].push_back(matrix[j][i]);
+        }
+    }
+
+    return new_matrix;
+}
+
+void printMatrix(vector<vector<char>> matrix)
+{
+    for( auto& line : matrix){
+        for ( auto& c : line)
+        {
+            cout << c;
+        }
+
+        cout << endl;        
+    }
 }
 
 int main(int argc, char* argv[]){
@@ -91,19 +157,15 @@ int main(int argc, char* argv[]){
 
     size_t count{0};
 
-    for (auto& line : chars)
-    {
-        for (auto& c : line)
-        {
-            cout << c;
-        }
-        cout << endl;
+    // printMatrix(chars);
+    count += countOccurrencesInDatabase(chars);
+    cout << endl;
 
-        size_t new_count = countOccurrences(line, line.begin(), 0);
-        cout << "Found " << new_count << endl;
-        count += new_count; 
-    }
-    
+    auto vertical_chars = getVerticalMatrixFrom(chars);
+    // printMatrix(vertical_chars);
+    count += countOccurrencesInDatabase(vertical_chars);
+    cout << endl;
+
     cout << "Count: " << count << endl;
 
 }
